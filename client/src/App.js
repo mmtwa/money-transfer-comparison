c776a0e3446ed100f3fe4683d2a066abc8e2b64d
mmtwa <mercedesclkexj@gmail.com>
2025-04-15 17:50:29 +0100

    Your commit message describing the changes

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App bg-white text-gray-900 min-h-screen">
       <MoneyTransferComparison />
     </div>
   );
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-      "./client/src/**/*.{js,jsx,ts,tsx}",
-      "./client/public/index.html"
+      "./src/**/*.{js,jsx,ts,tsx}",
+      "./public/index.html"
     ],
     theme: {
       extend: {},
