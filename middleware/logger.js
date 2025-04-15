f37ed47c6e5df51cb19d00dfc1ddae5a2d3be50e
mmtwa <mercedesclkexj@gmail.com>
2025-04-15 13:43:26 +0100

    Update logger.js

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,7 +1,4 @@
-// middleware/logger.js
-/**
- * Logs request to console
- */
+
 const logger = (req, res, next) => {
     console.log(
       `${req.method} ${req.protocol}://${req.get('host')}${req.originalUrl}`
