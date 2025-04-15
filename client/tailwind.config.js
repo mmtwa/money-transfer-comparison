55b2d8ba1f401af931edb012f04592a4caff8e8b
mmtwa <mercedesclkexj@gmail.com>
2025-04-15 17:14:23 +0100

    Your commit message describing the changes

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,8 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-      "./src/**/*.{js,jsx,ts,tsx}",
-      "./public/index.html"
+      "./client/src/**/*.{js,jsx,ts,tsx}",
+      "./client/public/index.html"
     ],
     theme: {
       extend: {},
