9c966e31c60c0f37a17845ba77c7046a7f79ee55
mmtwa <mercedesclkexj@gmail.com>
2025-04-16 02:42:16 +0100

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import MoneyTransferComparison from './MoneyTransferComparison.js';
+import MoneyTransferComparison from './MoneyTransferComparison';
 import './App.css';
 
 function App() {
