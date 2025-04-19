90ffbbd1b59b7aa2b85192672b04465efd52af23
mmtwa <mercedesclkexj@gmail.com>
2025-04-19 12:41:30 +0100

    Your commit message describing the changes

--- client/src/components/ModernMoneyCompare.js
+++ client/src/components/ModernMoneyCompare.js
@@ -519,7 +519,7 @@ const ResponsiveMoneyCompare = () => {
           <header className="bg-white border-b border-gray-200 shadow-sm sticky top-0 z-10 py-4">
             <div className="container mx-auto px-4 flex justify-center">
               <img 
-                src="/mmtlogo.png" 
+                src="/mmtlogo c.png" 
                 alt="mymoneytransfers Logo" 
                 className="h-8 md:h-10 cursor-pointer hover:opacity-80 transition" 
                 onClick={handleBackToHome}
