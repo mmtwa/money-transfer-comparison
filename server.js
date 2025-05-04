9043005c7f0b0f2b0cb65796e67abddda6a88f8a
mmtwa <mercedesclkexj@gmail.com>
2025-05-04 01:01:37 +0100

    Your commit message describing the changes

--- server.js
+++ server.js
@@ -80,7 +80,7 @@ app.use(helmet({
       "style-src": ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com"],
       "img-src": ["'self'", "data:", "https://www.google-analytics.com", "https://stats.g.doubleclick.net", "https://dq8dwmysp7hk1.cloudfront.net", "https://*.wise.com", "https://wise.com"],
       "font-src": ["'self'", "data:", "https://fonts.gstatic.com"],
-      "connect-src": ["'self'", "https://www.google-analytics.com", "https://*.google-analytics.com", "https://region1.google-analytics.com", "https://stats.g.doubleclick.net"]
+      "connect-src": ["'self'", "https://www.google-analytics.com", "https://*.google-analytics.com", "https://region1.google-analytics.com", "https://stats.g.doubleclick.net", "https://cdn.jsdelivr.net"]
     }
   },
   crossOriginEmbedderPolicy: false,  // Allow loading resources from different origins
